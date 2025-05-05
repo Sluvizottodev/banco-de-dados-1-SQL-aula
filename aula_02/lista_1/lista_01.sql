6a556a9332c0e4b16057b6149ae306558eae37f5
SLuvizotto <luvizottodev@gmail.com>
2025-05-04 23:49:18 -0300

    INSERT INTO --> Subindo dados de medicamentos

--- aula_02/lista_1/lista_01.sql
+++ aula_02/lista_1/lista_01.sql
@@ -12,3 +12,10 @@ CREATE TABLE medicamento (
     principio VARCHAR(100) NOT NULL, // principio ativo
 );
 
+INSERT INTO medicmento (descricao, preco_custo, estoque, fabricante, data_cadastro, pricipio)
+VALUES
+    ('SOMALIUM 6MG', 15.25, 28, 'ACHE', 10/09/2023, 'BROMAZEPAN'),
+    ('NOVALGINA GOTAS', 12.50, 50, 'HMS', 15/10/2024, 'DIPIRONA'),
+    ('TYLENOL 750MG', 14.00, 20, 'HMS', 15/10/2024, 'PARACETAMOL'),
+    ('CATAFLAN 50MG', 22.30, 40, 'NOVARTIS', now(), 'DICLOFENACO'),
+
