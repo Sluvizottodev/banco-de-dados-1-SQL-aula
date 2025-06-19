0fdafb6c034acb6f7f7b26c1f1b340b59ad13275
SLuvizotto <luvizottodev@gmail.com>
2025-06-18 23:59:21 -0300

    feat: Adiciona script de transação com operações de saldo e controle de autocommit

---
+++ aula_13_integridade/caseExercicio.sql
@@ -0,0 +1,27 @@
+-- 1) Considerando estoque normal a partir de 20, estoque crítico menor que 20 e estoque zerado com valor igual a zero, 
+-- listar id, descrição, estoque e a situacao do estoque de todos os produtos utilizando case.
+
+
+
+-- 2) Considerando estoque normal a partir de 20 e estoque crítico menor que 20 
+-- listar id, descrição, estoque e a situacao do estoque de todos os produtos utilizando IF ternário.
+
+
+-- 3) Considere as seguintes informações sobre as regiões do país
+-- SUL --> 'PR', 'RS', 'SC'
+-- SUDESTE --> 'SP', 'RJ', 'ES', 'MG'
+-- Centro-Oeste --> 'MT', 'MS', 'GO'
+-- Nordeste --> 'BA', 'AL', 'SE', 'PB', 'PE', 'RN', 'CE', 'PI', 'MA'
+-- Norte --> 'AM', 'RR', 'AP', 'PA', 'TO', 'RO', 'AC'
+-- Liste o nome do fornecedor, o nome da cidade, a sigla da UF e a região de todos os fornecedores
+
+
+
+-- 4) Listar o nome do fornecedor, a descrição do produto, o estoque e o preço de custo dos produtos 
+-- incluindo os fornecedores que não possuem produtos relacionados. Use case para ordenar. 
+-- Se o nome do produto for nulo, ordene pelo fornecedor. Caso contrário, ordene pelo estoque. 
+
+
+-- 5)Sem utilizar a função date_format,listar as datas completas(no formato dia/mes/ano) e a descrição do dia da semana dos dias em que houve vendas para clientes com o nome iniciado por A,C e H em 2006. Ordenar pela data da venda. Dica: use funções de data, case e subconsulta.
+
+-- 6) Utilizando a função date_format,listar as datas completas(no formato dd/mm/yyyy) e a descrição do dia da semana dos dias em que houve vendas para clientes com o nome iniciado por A,C e H em 2006. Ordenar pela data da venda. Dica: use funções de data, case e subconsulta.
