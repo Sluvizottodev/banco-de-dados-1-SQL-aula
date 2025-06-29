ec40d2c13ad2b91e1cf8c983e6e0765f8bcdabb1
SLuvizotto <luvizottodev@gmail.com>
2025-06-28 23:03:13 -0300

    feat: Adiciona script de subconsultas para análise de vendas

---
+++ aula_08/subconsulta_enum_exercicio.sql
@@ -0,0 +1,18 @@
+USE basevendasv2;
+-- Subconsultas. 
+-- Exemplo clientes que compraram em 2006
+
+-- Exemplo clientes da UF RJ
+
+-- Produtos vinculados a fornecedores inválidos
+
+-- Exemplo clientes de NOVA FRIBURGO que compraram produtos em quantidade acima de 20
+
+-- Exemplo produtosvendidos que não estão íntegros
+
+-- DELETE COM OUTER JOIN (produtosvendidos) IMPORTANTE: Faça um SELECT ANTES!
+
+-- apagando as inconsist~encias
+
+-- nenhuma inconsistência
+
