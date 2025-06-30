8171a39abad527a7c81b39f4daa7699929d5db91
SLuvizotto <luvizottodev@gmail.com>
2025-06-29 23:51:10 -0300

    feat: Adiciona script para criação da base de dados biblioteca2025  com integridade referencial

---
+++ aula_14/exercicio_integridade_referencial.sql
@@ -0,0 +1,11 @@
+--> Crie a base de dados biblioteca2025 e crie as tabelas INNODB abaixo implementando todas as foreign Keys e constraints necessárias para que a operação delete seja restrita e a atualização ocorra em cascata nas tabelas que dão origem às chaves estrangeiras. 
+#Dica: Comece pelas tabelas que não possuem chaves estrangeiras. - IMAGEM REFETENTE NA PASTA
+
+CREATE DATABASE biblioteca2025;
+USE biblioteca2025;
+-- GENEROS
+CREATE TABLE IF NOT EXISTS generos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(30) NOT NULL UNIQUE
+) ENGINE=INNODB;
+
