6f67e7d6b7b951b4880c587f2bc94a58f582ead9
SLuvizotto <luvizottodev@gmail.com>
2025-07-02 14:08:32 -0300

    feat: adiciona consulta para listar fornecedores com produtos em estoque acima da média em 2006

--- simulado/p2/ex_1.sql
+++ simulado/p2/ex_1.sql
@@ -1,2 +1,13 @@
- 
---1) Listar apenas o nome dos fornecedores que possuem produtos com estoque acima da média vendidos no ano de 2006.
+-- 1) Listar apenas o nome dos fornecedores que possuem produtos com estoque acima da média vendidos no ano de 2006.
+
+SELECT DISTINCT f.name
+FROM fornecedor f 
+JOIN produto p ON p.fornecedor_id = f.id
+JOIN vendas v ON v.produto_id = p.id 
+WHERE YEAR(v.data_venda) = 2006
+    AND p.estoque > (
+        SELECT AVG(p2.estoque)
+        FROM produtos p2
+        JOIN vendas v2 ON v2.produto_id = p2.id 
+        WHERE YEAR(v2.data_venda) = 2006
+    );
